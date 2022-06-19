b7dd05eeec5ffb5a28f5576cb981ae2502d3eec0
Raphael Gomes Santos <raphael.gomes@ibm.com>
2022-06-19 20:13:07 -0300

    Creating the models layer

--- server.js
+++ server.js
@@ -3,17 +3,20 @@
 const Hapi = require("@hapi/hapi");
 const Settings = require("./settings");
 const Routes = require("./lib/routes");
+const Models = require("./lib/models/");
 
 const init = async () => {
-  const server = new Hapi.Server({ port: Settings.port });
-  server.route(Routes);
-  await server.start();
-  console.log(`Server running at: ${server.info.uri}`);
+    const server = new Hapi.Server({ port: Settings.port });
+    server.route(Routes);
+
+    await Models.sequelize.sync();
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
 };
 
 process.on("unhandledRejection", err => {
-  console.log(err);
-  process.exit(1);
+    console.log(err);
+    process.exit(1);
 });
 
 init();
