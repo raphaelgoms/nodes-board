26fd190f486007158052bc8e8772e4af5ce5d21b
Raphael Gomes Santos <raphael.gomes@ibm.com>
2022-06-26 21:24:21 -0300

    Adding the viewa layer

--- server.js
+++ server.js
@@ -4,9 +4,22 @@ const Hapi = require("@hapi/hapi");
 const Settings = require("./settings");
 const Routes = require("./lib/routes");
 const Models = require("./lib/models/");
+const Path = require("path");
 
 const init = async () => {
     const server = new Hapi.Server({ port: Settings.port });
+    await server.register([require("@hapi/vision"), require("@hapi/inert")]);
+
+    server.views({
+        engines: { pug: require("pug") },
+        path: Path.join(__dirname, "lib/views"),
+        compileOptions: {
+            pretty: false
+        },
+        isCached: Settings.env === "production"
+    });
+
+    // Add routes
     server.route(Routes);
 
     await Models.sequelize.sync();
